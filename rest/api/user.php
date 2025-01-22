66c5629f96f7f6abf73702b7c818389b79c50234
bjt-00 <bitguiders@gmail.com>
2025-01-21 20:01:32 -0500

    27: user api crud operation added

--- rest/api/user.php
+++ rest/api/user.php
@@ -1,17 +1,26 @@
-[
- {
-	"firstName":"Abdul",
-	"lastName":"Kareem",
-	"orderCode":99
- },
- {
- 	"firstName":"Fatima",
- 	"lastName":"Jabbar"	
-	"ordercode":100
- },
- {
- 	"firstName":"Ayesha",
- 	"lastName":"Jabbar"
-	"orderCode":95
-  }
- ]
+<?php 
+
+include '../controller/UserController.php';
+
+header("Content-Type: application/json; charset = utf-8");
+header("Accept: application/json; charset = utf-8");
+
+$userController = new UserController();
+
+$requestMethod='REQUEST_METHOD';
+$request = file_get_contents('php://input');
+$request = json_decode($request);
+	
+if($_SERVER[$requestMethod] == "GET"){
+	echo $userController->getUserList();}
+else if($_SERVER['REQUEST_METHOD'] === 'POST'){
+	echo $userController->addUser($request->firstName,$request->lastName,$request->orderCode);
+}
+else if($_SERVER['REQUEST_METHOD'] === 'PUT'){
+	echo $userController->updateUser($request->firstName,$request->lastName,$request->orderCode);
+}
+else if($_SERVER['REQUEST_METHOD'] === 'DELETE'){
+	echo $userController->deleteUser($request->orderCode);
+}
+
+?>
